622c5142346b6bd94a774661d6e5f04228802fe8
Mustafa <mostafamo7med1210@gmail.com>
2026-04-17 21:01:18 +0200

    feature: add/hello route

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello from Multi-Stage Docker! 🚀');
 });
-
+app.get('/hello', (req, res) => {
+  res.send('Hello Mustafa! This is a new route 🎉');
+});
 app.listen(3000, () => {
   console.log('App running on port 3000');
 });
